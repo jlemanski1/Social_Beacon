309072878204234b1162e9312a0ffb85981c433c
Jonathan Lemanski <jlemanski1@gmail.com>
2019-12-07 19:34:00 -0700

    Implemented user searching base functionality

--- social_beacon/lib/pages/search.dart
+++ social_beacon/lib/pages/search.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:social_beacon/models/user.dart';
+import 'package:social_beacon/pages/home.dart';
+import 'package:social_beacon/widgets/progress.dart';
 
 class Search extends StatefulWidget {
   @override
@@ -7,24 +11,42 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
+  TextEditingController searchController = TextEditingController();
+  Future<QuerySnapshot> searchResults;
+
+  // Takes input from the appbar search formfield and processes it
+  handleSearch(String query) {
+    Future<QuerySnapshot> users = usersRef
+    .where('displayName', isGreaterThanOrEqualTo: query)
+    .getDocuments();
+
+    setState(() {
+      searchResults = users;
+    });
+  }
+
+  // Clears the search text formfield
+  clearSearch() {
+    searchController.clear();
+  }
 
   // Build App bar search field
   AppBar buildSearchField() {
     return AppBar(
       backgroundColor: Colors.white,
       title: TextFormField(
+        controller: searchController,
         decoration: InputDecoration(
           hintText: 'Search for a user...',
           filled: true,
           prefixIcon: Icon(Icons.account_box, size: 28.0,),
           suffixIcon: IconButton(
             icon: Icon(Icons.clear),
-            onPressed: () {
-              print('cleared\n');
-            },
+            onPressed: clearSearch,
           )
         ),
-      )
+        onFieldSubmitted: handleSearch,
+      ),
     );
   }
 
@@ -59,15 +81,38 @@ class _SearchState extends State<Search> {
     );
   }
 
+  // Display User docs that are retrieved from cloud firestore
+  FutureBuilder buildSearchResults() {
+    return FutureBuilder(
+      future: searchResults,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return circularProgress();
+        } else {
+          List<Text> searchResultsList = [];
+          snapshot.data.documents.foreach((doc) {
+            User user = User.fromDocument(doc);
+            searchResultsList.add(Text(user.username));
+          });
+          return ListView(
+            children: searchResultsList,
+          );
+        }
+      }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor.withOpacity(0.8),
       appBar: buildSearchField(),
-      body: buildNoContent(),
+      body: searchResults == null ? buildNoContent() : buildSearchResults(),
     );
   }
 }
+
+
 
 class UserResult extends StatelessWidget {
   @override
