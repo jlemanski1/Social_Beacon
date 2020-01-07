ad7a0a694bfcd56119654dca4d6c5af52094ed19
Jonathan Lemanski <jlemanski1@gmail.com>
2020-01-07 16:45:25 -0700

    Added overflow to Header, started on post screen

--- social_beacon/lib/pages/post_screen.dart
+++ social_beacon/lib/pages/post_screen.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:social_beacon/pages/home.dart';
 
 
 class PostScreen extends StatelessWidget {
+  final String userId;
+  final String postId;
+
+  PostScreen({this.postId, this.userId});
+
+
   @override
   Widget build(BuildContext context) {
-    return Text(
-      'Post Screen'
-    );
+    return FutureBuilder(
+      future: postsRef.document(userId).collection('userPosts').document(postId).get(),
+    )
   }
 }
--- social_beacon/lib/widgets/header.dart
+++ social_beacon/lib/widgets/header.dart
@@ -10,6 +10,7 @@ AppBar header(context, { bool isAppTitle = false, String titleText, rmBackButton
         fontFamily: isAppTitle ? 'Signatra' : '',
         fontSize: isAppTitle ? 50.0 : 22.0,
       ),
+      overflow: TextOverflow.ellipsis,
     ),
     centerTitle: true,
     backgroundColor: Theme.of(context).primaryColor,
